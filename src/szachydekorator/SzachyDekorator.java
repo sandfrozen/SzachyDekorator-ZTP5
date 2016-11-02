6a0113a5e3c0add1ecff84d6559d0a1ee7518313
Tomasz Busłowski <tbuslowski@gmail.com>
2016-11-02 12:58:37 +0100

    szachy początek

---
+++ src/szachydekorator/SzachyDekorator.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package szachydekorator;
+
+/**
+ *
+ * @author A5US
+ */
+public class SzachyDekorator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
